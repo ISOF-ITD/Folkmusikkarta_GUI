737d934a713939e849740e776f338bd276edd63b
Rico Simke <rico.simke@isof.se>
2020-05-12 15:32:57 +0200

    added langData.js

--- scripts/lang/langData.js
+++ scripts/lang/langData.js
@@ -1,2 +1,9 @@
 export default {
+	'sv': {
+		'Mina sägner': 'Mina insamlingar',
+		'Inga sparade sägner': 'Inga sparade insamlingar',
+		'Klicka på stjärnan för att spara sägner till din egen lista.': 'Klicka på stjärnan för att spara insamlingar till din egen lista.',
+		'har sparats till dina sägner': 'har sparats till dina insamlingar',
+		'Klicka här för att visa lista över dina sparade uppteckningar.': 'Klicka här för att visa lista över dina sparade insamlingar.'
+	}
 }
